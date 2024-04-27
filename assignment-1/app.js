a6d663c3a101605cb638d348b789789f3b2cbfb6
John Douma <johndouma@Johns-MBP.home>
2024-04-27 12:51:07 -0400

    Assignment-1
    Added the basic functionality. I did not opt for the color selections.

--- assignment-1/app.js
+++ assignment-1/app.js
@@ -4,8 +4,21 @@
     angular.module("LunchCheck", [])
     .controller("LunchCheckController", LunchCheckController);
 
-    LunchCheckController.$inject($scope);
     function LunchCheckController($scope) {
+        $scope.message = "";
+        $scope.foodList = "";
+        $scope.checkFood = function() {
+            if ($scope.foodList == '') {
+                $scope.message = "Please enter data first";
+                return;
+            }
+
+            if (list.length > 3) {
+                $scope.message = "Too much!"
+            } else {
+                $scope.message = "Enjoy!";
+            }
+        };
 
     }
 })();
